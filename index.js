e74b56e3ae0dae03632fa52030f5f5d84c7905c3
Catherine Gloria <catherinegloria@gmail.com>
2022-02-04 07:42:28 -0800

    data type examples

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 //Data Types
 
 //string
+let myString = 'string';
+console.log(`This is a ${myString}.`)
+
 //number
+let num = 5;
+num += 1;
+console.log(`One plus five equals ${num}.`)
+
 //boolean
+let myBoolean = true;
+console.log('The sky is blue.')
+console.log(`This is ${myBoolean}.`)
+
 //null
 //undefined
 //symbol
